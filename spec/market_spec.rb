a10cd1d30d0ebf148115a1653275c6cb71265e4c
Antoine Aube <aaube3@gmail.com>
2023-08-08 09:43:31 -0600

    test: #add_vendor

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -19,4 +19,22 @@ RSpec.describe Market do
       expect(market.vendors).to eq([])
     end
   end
+
+  describe "#add_vendor" do
+    it "can add vendors to its vendors array" do 
+      vendor1.stock(item1, 35)
+
+      vendor1.stock(item2, 7)
+      vendor2.stock(item4, 50)
+
+      vendor2.stock(item3, 25)
+      vendor3.stock(item1, 65)
+
+      market.add_vendor(vendor1)
+      market.add_vendor(vendor2)
+      market.add_vendor(vendor3)
+
+      expect(market.vendors).to eq([vendor1, vendor2, vendor3])
+    end
+  end
 end
