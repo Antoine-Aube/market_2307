f03da9bd6f4302b24f880ec66bb801c1b62f0841
Antoine Aube <aaube3@gmail.com>
2023-08-08 09:23:13 -0600

    test: #check_stock and #stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Item do
     it{ expect(vendor.name).to eq("Rocky Mountain Fresh") }
     it{ expect(vendor.inventory).to eq({}) }
   end
+
+  describe "#check_stock and #stock" do
+    it "can check stock given at item as an argument" do 
+      expect(vendor.check_stock(item1)).to eq(0)
+      
+      vendor.stock(item1, 30)
+      
+      expect(vendor.inventory).to eq({item1 => 30})
+      
+      expect(vendor.check_stock(item1)).to eq(30)
+      
+      vendor.stock(item1, 25)
+      
+      expect(vendor.check_stock(item1)).to eq(55)
+      
+      vendor.stock(item2, 12)
+
+      expect(vendor.inventory).to eq({item1 => 30, item2 => 12})
+    end
+  end
 end 
