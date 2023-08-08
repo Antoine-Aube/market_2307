2bc4f840ef3168e5da7707ccefda9c0c1ff8320b
Antoine Aube <aaube3@gmail.com>
2023-08-08 09:36:50 -0600

    test: Market initialize

---
+++ spec/market_spec.rb
@@ -0,0 +1,22 @@
+require './lib/item'
+require './lib/vendor'
+require './lib/market'
+
+RSpec.desribe Market do 
+  let(:item1) { Item.new({name: 'Peach', price: "$0.75"}) }
+  let(:item2) { Item.new({name: 'Tomato', price: '$0.50'}) }
+  let(:item3) { Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"} }
+  let(:item4) { Item.new({name: "Banana Nice Cream", price: "$4.25"} }
+  let(:vendor1) { Vendor.new("Rocky Mountain Fresh") }
+  let(:vendor2) { Vendor.new("Ba-Nom-a-Nom") }
+  let(:vendor3) { Vendor.new("Palisade Peach Shack") }
+  let(:market) { Market.new("South Pearl Street Farmers Market") }
+
+  describe "#initialize" do 
+    it "exists and has attributes" do 
+      expect(market).to be_a Market
+      expect(market.name).to eq("South Pearl Street Farmers Market")
+      expect(market.vendors).to eq([])
+    end
+  end
+end
--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Item do
       expect(vendor.check_stock(item1)).to eq(55)
       
       vendor.stock(item2, 12)
-
+      
       expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
     end
   end
